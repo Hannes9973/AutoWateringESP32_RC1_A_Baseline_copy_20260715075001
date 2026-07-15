d5cccffc075c8637c698f6f7329d5f6ef7154260
Hannes9973 <xfrank.schmitt@web.de>
2026-07-15 19:38:23 +0200

    RC1-B.19 Add detailed pot diagnostic command
    - Added "pot <n>" command
    - Display state, weight, start and target weight
    - Display auto mode
    - Display pump state
    - Display watering counter
    - Display error counter

--- AutoWateringESP32_RC1_A_Baseline_copy_20260715075001/CommandManager.cpp
+++ AutoWateringESP32_RC1_A_Baseline_copy_20260715075001/CommandManager.cpp
@@ -22,7 +22,10 @@ bool CommandManager::equals(const char* text) const
 {
     return strcmp(_buffer,text)==0;
 }
-
+bool CommandManager::startsWith(const char* text) const
+{
+    return strncmp(_buffer, text, strlen(text)) == 0;
+}
 void CommandManager::printHelp() const
 {
     Serial.println("help");
@@ -57,26 +60,107 @@ void CommandManager::processCommand(ScaleManager& scale,
     else if(equals("version")) printVersion();
     else if(equals("info")) printInfo();
     else if(equals("status"))
+{
+    for(uint8_t i = 0; i < NUMBER_OF_POTS; i++)
     {
-        for(uint8_t i=0;i<NUMBER_OF_POTS;i++)
-        {
-            Serial.print("Topf ");
-            Serial.print(i+1);
-            Serial.print(": ");
-            Serial.print(scale.getWeight(i),1);
-Serial.print(" g ");
+        Serial.print("Topf ");
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.print(scale.getWeight(i), 1);
+        Serial.print(" g ");
 
-Serial.print("State=");
-Serial.print(pot[i].getStateName());
-Serial.print(" Waterings=");
-Serial.print(pot[i].getStatus().wateringCount);
+        Serial.print("State=");
+        Serial.print(pot[i].getStateName());
 
-Serial.print(" Errors=");
-Serial.print(pot[i].getErrorCount());
-Serial.print(" Pump=");
-Serial.println(pump.isRunning(i) ? "EIN" : "AUS");
-        }
+        Serial.print(" Waterings=");
+        Serial.print(pot[i].getStatus().wateringCount);
+
+        Serial.print(" Errors=");
+        Serial.print(pot[i].getErrorCount());
+
+        Serial.print(" Pump=");
+        Serial.println(pump.isRunning(i) ? "EIN" : "AUS");
     }
+}else if(startsWith("pot "))
+{
+    int potIndex = atoi(_buffer + 4) - 1;
+
+    if(potIndex < 0 || potIndex >= NUMBER_OF_POTS)
+    {
+        Serial.println("Ungueltige Topfnummer");
+    }
+    else
+    {
+        Serial.print("Topf ");
+        Serial.println(potIndex + 1);
+         Serial.print("State: ");
+    Serial.println(pot[potIndex].getStateName());
+    Serial.print("Weight: ");
+Serial.print(pot[potIndex].getWeight(), 1);
+Serial.println(" g");
+Serial.print("Start: ");
+Serial.print(pot[potIndex].getStartWeight(),1);
+Serial.println(" g");
+
+Serial.print("Target: ");
+Serial.print(pot[potIndex].getTargetWeight(), 1);
+Serial.println(" g");
+
+Serial.print("Auto: ");
+Serial.println(pot[potIndex].getAutoMode() ? "ON" : "OFF");
+
+Serial.print("Pump: ");
+Serial.println(pump.isRunning(potIndex) ? "ON" : "OFF");
+
+Serial.print("Waterings: ");
+Serial.println(pot[potIndex].getStatus().wateringCount);
+
+Serial.print("Errors: ");
+Serial.println(pot[potIndex].getErrorCount());
+    }
+}
+
+else if(startsWith("pot "))
+{
+    int potIndex = atoi(_buffer + 4) - 1;
+
+    if(potIndex < 0 || potIndex >= NUMBER_OF_POTS)
+    {
+        Serial.println("Ungueltige Topfnummer");
+    }
+    else
+{
+    Serial.print("Topf ");
+    Serial.println(potIndex + 1);
+
+    Serial.print("State: ");
+    Serial.println(pot[potIndex].getStateName());
+
+    Serial.print("Weight: ");
+Serial.print(pot[potIndex].getWeight(), 1);
+Serial.println(" g");
+Serial.print("Start: ");
+Serial.print(pot[potIndex].getStartWeight(), 1);
+Serial.println(" g");
+Serial.print("Auto: ");
+Serial.println(pot[potIndex].getAutoMode() ? "ON" : "OFF");
+
+Serial.print("Pump: ");
+Serial.println(pump.isRunning(potIndex) ? "ON" : "OFF");
+
+Serial.print("Waterings: ");
+Serial.println(pot[potIndex].getStatus().wateringCount);
+
+Serial.print("Errors: ");
+Serial.println(pot[potIndex].getErrorCount());
+
+}
+
+        
+    
+}
+
+else if(equals("tare 1")) scale.tare(0);
     else if(equals("tare 1")) scale.tare(0);
     else if(equals("tare 2")) scale.tare(1);
     else if(equals("tare 3")) scale.tare(2);
